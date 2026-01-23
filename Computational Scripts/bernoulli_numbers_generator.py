56416aacff8b987c3aa096c5694bd03bfbf965df
rileymoriss <75900562+rileymoriss@users.noreply.github.com>
2026-01-23 20:56:57 +1100

    Bernoulli script complete

--- Computational Scripts/bernoulli_numbers_generator.py
+++ Computational Scripts/bernoulli_numbers_generator.py
@@ -3,47 +3,59 @@
 
 from fractions import Fraction
 #Fraction(value).denominator to get the denominator of a rational number
-from sympy import sieve, prime
+from sympy import sieve, prime, bernoulli
 #all primes less than 19, [i for i in sieve.primerange(19)]
 import math
 
 
 def bernoulli_numbers_total(n, switch):
-    return
+    #For the non-denominator parts we use sympy's built in bernoulli number function, and translate it to the topologists indexing
+    bernoulli_nums = []
+    for i in range(1,n+1):
+        bernoulli_nums.append((-1)**(i+1)*bernoulli(2*i))
 
-def bernoulli_numbers_numerator(n, switch):
-    return
+    #Output to file if switch is on
+    if switch:
+        with open("output.txt", "w") as f:
+            f.write(str(bernoulli_nums))
+    return bernoulli_nums
 
 def bernoulli_numbers_denominator(n, switch):
     #This algorithm is suggested by Milnor-Stasheff Appendix B
+    #These are easier to compute and do not grow as fast as the numerators
     bernoulli_nums = []
     primes = [i for i in sieve.primerange(2*n)]
-    print("Inside the function, primes=", primes)
 
     for k in range(1, n+1):
-        print("k=", k)
         prime_factors = []
         for p in primes:
-            print("prime factors", prime_factors)
             if  (2*k)%(p-1) == 0:
                 i=0
                 while (k % (p**i) == 0):
                     i += 1
-                    print("inside while p=", p, " i=", i)
                 prime_factors.append(p**i)
-                print("prime factors", prime_factors)
-
-        print("prime_factors=", prime_factors)
         bernoulli_nums.append(math.prod(prime_factors)//math.gcd(k, math.prod(prime_factors)))
-        print("product of primes: ",math.prod(prime_factors))
-        print("dividing: ", k/(math.prod(prime_factors)))
-        print("denominator: ", Fraction(k/(math.prod(prime_factors))).denominator)
-        print("bernoulli_nums=", bernoulli_nums)
+
     
     
     #Output to file if switch is on
     if switch:
         with open("output.txt", "w") as f:
-            for num in bernoulli_nums:
-                f.write(str(num))
+            f.write(str(bernoulli_nums))
     return bernoulli_nums
+
+def bernoulli_numbers_numerator(n, switch):
+    #We use the previous two functions to compute the numerators
+    bernoulli_nums = []
+    denoms = bernoulli_numbers_denominator(n, False)
+    nums = bernoulli_numbers_total(n, False)
+
+    for i in range(len(nums)):
+        bernoulli_nums.append(nums[i]*denoms[i])
+
+    #Output to file if switch is on
+    if switch:
+        with open("output.txt", "w") as f:
+            f.write(str(bernoulli_nums))
+    return bernoulli_nums
+
